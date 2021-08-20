280acc27cce13d0f96d37438d125e2909a08eb16
Uhlmann, Jonathan <jonathan.uhlmann@wolterskluwer.com>
2021-08-20 11:09:01 +0200

    initial

---
+++ src/object/Hat.java
@@ -0,0 +1,9 @@
+package object;
+
+public class Hat {
+    private int amountCapturedHats;
+    private boolean isCaptured;
+    private int id;
+    private int playerId;
+
+}
---
+++ src/object/Player.java
@@ -0,0 +1,5 @@
+package object;
+
+public class Player {
+    private int activeHats;
+}
