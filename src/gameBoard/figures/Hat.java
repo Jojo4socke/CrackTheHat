46f1da976964f90d16132f85be46f562cbca8d5b
doVe829 <dvennegeerts@gmail.com>
2021-09-04 13:53:11 +0200

    fixed typo

--- src/gameBoard/figures/Hat.java
+++ src/gameBoard/figures/Hat.java
@@ -18,11 +18,11 @@ public class Hat extends JFrame {
         g2d.fillOval(150, 150, 100, 100);
     }
 
-    private  void createCapuredHatsString(Graphics g, String capuredHats){
+    private  void createCapturedHatsString(Graphics g, String capturedHats){
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(Color.BLACK);
         g.setFont(new Font("TimesRoman", Font.BOLD, 50));
-        g2d.drawString(capuredHats, 185, 210);
+        g2d.drawString(capturedHats, 185, 210);
 
     }
 
@@ -46,7 +46,7 @@ public class Hat extends JFrame {
         colourMapper.put("pink", Color.pink);
 
         createPlayerHat(g, colourMapper.get(hatColour));
-        createCapuredHatsString(g,numCapturedHats);
+        createCapturedHatsString(g,numCapturedHats);
     }
 
     public static void main(String[] args) {
