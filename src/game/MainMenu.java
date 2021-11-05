ba22face1d184d6786ae8761a132fb22e2983381
Uhlmann, Jonathan <jonathan.uhlmann@wolterskluwer.com>
2021-11-05 11:42:50 +0100

    refactoring

--- src/game/MainMenu.java
+++ src/game/MainMenu.java
@@ -9,24 +9,18 @@ import java.util.List;
 public class MainMenu extends JFrame{
 
     JPanel panel;
-<<<<<<< HEAD
-    JButton startButton
-=======
->>>>>>> 0d99718db51a1f4cb1b9f744c4998c55b51a4b90
+    JButton startButton;
+
 
     public MainMenu() {
         setTitle("CrackTheHat");
         setSize(400, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         panel = new JPanel();
-<<<<<<< HEAD
-
-=======
         Listener listener = new Listener(this);
         panel.add(listener.startButton);
         panel.add(listener.exitButton);
         add(panel);
->>>>>>> 0d99718db51a1f4cb1b9f744c4998c55b51a4b90
     }
 
     private void render() {
@@ -37,11 +31,4 @@ public class MainMenu extends JFrame{
     public void tick() {
         render();
     }
-
-<<<<<<< HEAD
-    public void () {
-
-    }
-=======
->>>>>>> 0d99718db51a1f4cb1b9f744c4998c55b51a4b90
 }
