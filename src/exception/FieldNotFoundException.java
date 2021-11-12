2d2ddebcbc998b448d1559371b16114b5048f630
Grauer, Vanessa <vanessa.grauer@wolterskluwer.com>
2021-11-12 12:24:37 +0100

    description for exceptions added

--- src/exception/FieldNotFoundException.java
+++ src/exception/FieldNotFoundException.java
@@ -1,6 +1,14 @@
 package exception;
 
+/**
+ *
+ * FieldNotFoundException is thrown when the fieldnumber doesn't exist.
+ *
+ * @author Cedric Nees, Daniel Schedek, Dominik Vennegeerts, Jonathan Uhlmann, Vanessa Grauer
+ * @version 2021-11-11
+ */
 public class FieldNotFoundException extends Exception {
+    // Constructor
     public FieldNotFoundException(String msg) {
         super(msg);
     }
--- src/exception/InvalidMoveException.java
+++ src/exception/InvalidMoveException.java
@@ -1,6 +1,17 @@
 package exception;
 
+/**
+ *
+ * InvalidMoveException is thrown when:<br/>
+ * 1. Maximum game pieces on a SaveField are reached<br/>
+ * 2. Game piece tries to join another players HomeField<br/>
+ * 3. The chosen number for the next field isn't a neighbour
+ *
+ * @author Cedric Nees, Daniel Schedek, Dominik Vennegeerts, Jonathan Uhlmann, Vanessa Grauer
+ * @version 2021-11-11
+ */
 public class InvalidMoveException extends Exception {
+    // Constructor
     public InvalidMoveException(String msg) {
         super(msg);
     }
